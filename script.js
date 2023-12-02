624dbdddece14a25f907d17de9ebabb7f471a459
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-12-02 22:07:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	var mailformat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+	if(str.value.match(mailformat))
+	{
+		return false;
+	}
 }
 
 // Do not change the code below.
